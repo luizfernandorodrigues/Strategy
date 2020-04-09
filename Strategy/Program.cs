1ddee1e97d0aeb9b94c1a0f5a5598f01797f056d
Luiz Fernando <luizfernando_rodrigues12@hotmail.com>
2020-04-09 20:13:51 -0300

    Add project files.

---
+++ Strategy/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList studentsRecords = new SortedList();
+
+            studentsRecords.Add("Luiz Fernando");
+            studentsRecords.Add("Lot");
+            studentsRecords.Add("Gilmar");
+            studentsRecords.Add("Fábio");
+            studentsRecords.Add("Mateus");
+            studentsRecords.Add("Gaspar");
+            studentsRecords.Add("Leandro");
+            studentsRecords.Add("Willian");
+
+            studentsRecords.SetSortStrategy(new QuickSort());
+            studentsRecords.Sort();
+
+            studentsRecords.SetSortStrategy(new ShellSort());
+            studentsRecords.Sort();
+
+            studentsRecords.SetSortStrategy(new MergeSort());
+            studentsRecords.Sort();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Strategy/SortedList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class SortedList
+    {
+        private List<string> _list = new List<string>();
+        private SortStrategy _sortedStrategy;
+
+        public void SetSortStrategy(SortStrategy sortStrategy)
+        {
+            _sortedStrategy = sortStrategy;
+        }
+
+        public void Add(string nome)
+        {
+            _list.Add(nome);
+        }
+
+        public void Sort()
+        {
+            _sortedStrategy.Sort(_list);
+
+            foreach (var item in _list)
+            {
+                Console.WriteLine($" {item}");
+            }
+        }
+    }
+}
